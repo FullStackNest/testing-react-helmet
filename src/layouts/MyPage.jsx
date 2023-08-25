13290eb86b460c5e23d120dcf588e69a2ae7b010
Prakash7143 <kumarprakash7143@gmail.com>
2023-08-25 20:03:59 +0530

    retesting

--- src/layouts/MyPage.jsx
+++ src/layouts/MyPage.jsx
@@ -1,11 +1,24 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import ImageFileOne from '../assets/BGF.jpg';
+// import ImageFileOne from '../assets/BGF.jpg';
+import { useEffect } from 'react';
+
+// Assuming you have fetched the description and image URL from the database
+const pageTitle = 'Flower Aura Shop';
+const pageDescription = 'Your page description goes here';
+const pageImageURL = 'https://images.pexels.com/photos/14120220/pexels-photo-14120220.jpeg';
 
 const MyPage = () => {
-    // Assuming you have fetched the description and image URL from the database
-    const pageTitle = 'Your Page Title';
-    const pageDescription = 'Your page description goes here';
-    const pageImageURL = 'https://images.pexels.com/photos/14120220/pexels-photo-14120220.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load';
+    useEffect(() => {
+        // Update the document's title when the component mounts
+        document.title = "About Us | Flower Aura Shop";
+
+
+        // Clean up the title when the component unmounts
+        return () => {
+            document.title = pageTitle;
+
+        };
+    }, [])
 
     return (
         <HelmetProvider>
@@ -22,7 +35,7 @@ const MyPage = () => {
                     <meta property="og:image" content={pageImageURL} />
                     <meta property="og:image:alt" content={pageTitle} />
                     <meta property="og:url" content={window.location.href} />
-                    <meta property="og:type" content="website" />
+                    <meta property="og:type" content="https://app-share.netlify.app/page" />
                 </Helmet>
             </div>
         </HelmetProvider>
